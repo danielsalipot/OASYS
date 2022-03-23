08b7f1bbb8197dab970cef662bd692eb348f3344
Daniel Salipot <danielsalipot@gmail.com>
2022-03-23 21:05:02 +0800

    Edited index page and controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    function index(){
+        return view('pages.index');
+    }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layout.app')
     @section('content')
-        <h1> Hello world!</h1>
-        <h2> Hello Arnold!</h2>
+    <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col p-5 justify-content-center text-center text-primary">
+            <h1 class="pt-5 mt-5">OASYS</h1>
+            <p>Overall Administering System</p>
+            <p class="m-auto w-75 mt-5 pt-3 mb-5 ">A Human Resource Records and Information Management System. 
+                a tool that is designed to assist firms in meeting their key Human 
+                Resource requirements while also increasing the efficiency of 
+                both management and staff.</p>
+            <button type="button " class="btn btn-primary w-25 m-1">LOGIN</button>
+            <br>
+            <button type="button" class="btn btn-outline-primary w-25 m-1">SEND APPLICATION</button>
+        </div>
+    </div>
     @endsection
