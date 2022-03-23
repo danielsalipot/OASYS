9d108b1e505491396f3b507ebff34de9ada05dfe
Nehemiah Manalili <nehemiahmanalili@gmail.com>
2022-03-23 22:27:12 +0800

    Applicant Pathing

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,5 +8,9 @@ class PagesController extends Controller
 {
     function index(){
         return view('pages.index');
+        
+    }
+    function applicants(){
+        return view('pages.applicants.index');
     }
 }
