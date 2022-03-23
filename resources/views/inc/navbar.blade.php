c4218c6b69ab30f89a2fff5e3d43889c632017a7
unknown <arnoldadriel.tarrega@gmail.com>
2022-03-23 21:42:58 +0800

    Navbar Edit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-4">Simple header</span>
+    </a>
+
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="/index" class="nav-link active" aria-current="page">LARAVEL</a></li>
+      <li class="nav-item"><a href="/index" class="nav-link">HOME</a></li>
+      <li class="nav-item"><a href="/about" class="nav-link">ABOUT</a></li>
+      <li class="nav-item"><a href="/services" class="nav-link">SERVICE</a></li>
+    </ul>
+  </header>
