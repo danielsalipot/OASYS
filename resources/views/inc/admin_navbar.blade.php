0dff75c4983ef38b88fe866774cd15269e0cdc20
unknown <arnoldadriel.tarrega@gmail.com>
2022-04-10 16:09:50 +0800

    Navbar Dropdown Modified

--- resources/views/inc/admin_navbar.blade.php
+++ resources/views/inc/admin_navbar.blade.php
@@ -8,28 +8,28 @@
         <li><a href="/attendance" class="nav-link py-3" title="Attendance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-calendar-check"></i></a></li>
         <li><a href="/performance" class="nav-link py-3 pb-4" title="Performance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bar-chart-line-fill"></i></a></li>
         <div class="dropdown">
-          <button class="btn col-md-12 text-center py-2" type="button" id="dropdown1" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn col-md-12 text-center py-2" type="button" data-bs-toggle="dropdown">
             <i class="bi bi-journal-bookmark"></i>
           </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdown1">
-            <li><a class="dropdown-item" href="/moduleorientation">Module</a></li>
-            <li><a class="dropdown-item" href="/peopleorientation">People</a></li>
-          </ul>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="/moduleorientation">Module</a>
+            <a class="dropdown-item" href="/peopleorientation">People</a>
+          </div>
         </div>
         <div class="dropdown">
-          <button class="btn col-md-12 text-center py-2" type="button" id="dropdown2" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn col-md-12 text-center py-2" type="button" data-bs-toggle="dropdown">
             <i class="bi bi-briefcase-fill"></i>
           </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdown2">
+          <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="/moduletraining">Module</a></li>
             <li><a class="dropdown-item" href="/peopletraining">People</a></li>
           </ul>
         </div>
         <div class="dropdown">
-          <button class="btn col-md-12 text-center py-2" type="button" id="dropdown3" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn col-md-12 text-center py-2" type="button" data-bs-toggle="dropdown">
             <i class="bi bi-wrench"></i>
           </button>
-          <ul class="dropdown-menu" aria-labelledby="dropdown3">
+          <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="/modulecorrection">Module</a></li>
             <li><a class="dropdown-item" href="/peoplecorrection">People</a></li>
           </ul>
