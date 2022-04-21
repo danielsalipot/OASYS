ac0b5cfbc73cedaff418ceecc0b89c6654417947
Nehemiah Manalili <nehemiahmanalili@gmail.com>
2022-04-22 00:43:33 +0800

    Refactor some controllers

--- resources/views/inc/admin_navbar.blade.php
+++ resources/views/inc/admin_navbar.blade.php
@@ -4,16 +4,16 @@
 
     <nav id="navbar" class="nav-menu navbar">
       <ul>
-        <li><a href="/adminhome"  class="nav-link py-3" title="Home" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-house-door-fill"></i></a></li>
-        <li><a href="/attendance" class="nav-link py-3" title="Attendance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-calendar-check"></i></a></li>
-        <li><a href="/performance" class="nav-link py-3 pb-4" title="Performance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bar-chart-line-fill"></i></a></li>
+        <li><a href="/admin/home"  class="nav-link py-3" title="Home" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-house-door-fill"></i></a></li>
+        <li><a href="/admin/attendance" class="nav-link py-3" title="Attendance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-calendar-check"></i></a></li>
+        <li><a href="/admin/performance" class="nav-link py-3 pb-4" title="Performance" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bar-chart-line-fill"></i></a></li>
         <div class="dropdown">
           <button class="btn col-md-12 text-center py-2" type="button" data-bs-toggle="dropdown">
             <i class="bi bi-journal-bookmark"></i>
           </button>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="/moduleorientation">Module</a>
-            <a class="dropdown-item" href="/peopleorientation">People</a>
+            <a class="dropdown-item" href="/admin/moduleorientation">Module</a>
+            <a class="dropdown-item" href="/admin/peopleorientation">People</a>
           </div>
         </div>
         <div class="dropdown">
@@ -21,8 +21,8 @@
             <i class="bi bi-briefcase-fill"></i>
           </button>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/moduletraining">Module</a></li>
-            <li><a class="dropdown-item" href="/peopletraining">People</a></li>
+            <li><a class="dropdown-item" href="/admin/moduletraining">Module</a></li>
+            <li><a class="dropdown-item" href="/admin/peopletraining">People</a></li>
           </ul>
         </div>
         <div class="dropdown">
@@ -30,13 +30,13 @@
             <i class="bi bi-wrench"></i>
           </button>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/modulecorrection">Module</a></li>
-            <li><a class="dropdown-item" href="/peoplecorrection">People</a></li>
+            <li><a class="dropdown-item" href="/admin/modulecorrection">Module</a></li>
+            <li><a class="dropdown-item" href="/admin/peoplecorrection">People</a></li>
           </ul>
-        </div>       
+        </div>
 
-        <li><a href="/adminmessage" class="nav-link py-3 pt-1" title="Messages" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-chat-left-text"></i></a></li>
-        <li><a href="/adminnotification" class="nav-link py-3" title="Notifications" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bell"></i></a></li>      </ul>
+        <li><a href="/admin/message" class="nav-link py-3 pt-1" title="Messages" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-chat-left-text"></i></a></li>
+        <li><a href="/admin/notification" class="nav-link py-3" title="Notifications" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bell"></i></a></li>      </ul>
       <ul>
         <li><a href="/logout" class="nav-link pt-5 mt-5" title="Logout" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-box-arrow-left"></i></a></li>
       </ul>
--- resources/views/inc/staff_navbar.blade.php
+++ resources/views/inc/staff_navbar.blade.php
@@ -4,16 +4,16 @@
 
     <nav id="navbar" class="nav-menu navbar">
       <ul>
-        <li><a href="/staffhome"  class="nav-link py-3 pt-0" title="Home" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-house-door-fill"></i></a></li>
-        <li><a href="/onboarding"  class="nav-link py-3" title="Onboarding" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-check-fill"></i></a></li>
-        <li><a href="/termination" class="nav-link py-3" title="Termination/Resignation" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-dash-circle-fill"></i></a></li>
-        <li><a href="/offboarding" class="nav-link py-3" title="Offboarding" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-x-fill"></i></a></li>
-        <li><a href="/schedules" class="nav-link py-3" title="Schedules" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-calendar2-week"></i></a></li>
-        <li><a href="/interview" class="nav-link py-3" title="Interviews" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-file-earmark-person-fill"></i></a></li>
-        <li><a href="/department" class="nav-link py-3" title="Departments" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-building"></i></a></li>
-        <li><a href="/position" class="nav-link py-3" title="Positions" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-workspace"></i></a></li>
-        <li><a href="/staffmessage" class="nav-link py-3" title="Messages" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-chat-left-text"></i></a></li>
-        <li><a href="/staffnotification" class="nav-link py-3" title="Notifications" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bell"></i></a></li>
+        <li><a href="/staff/home"  class="nav-link py-3 pt-0" title="Home" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-house-door-fill"></i></a></li>
+        <li><a href="/staff/onboarding"  class="nav-link py-3" title="Onboarding" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-check-fill"></i></a></li>
+        <li><a href="/staff/termination" class="nav-link py-3" title="Termination/Resignation" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-dash-circle-fill"></i></a></li>
+        <li><a href="/staff/offboarding" class="nav-link py-3" title="Offboarding" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-x-fill"></i></a></li>
+        <li><a href="/staff/schedules" class="nav-link py-3" title="Schedules" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-calendar2-week"></i></a></li>
+        <li><a href="/staff/interview" class="nav-link py-3" title="Interviews" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-file-earmark-person-fill"></i></a></li>
+        <li><a href="/staff/department" class="nav-link py-3" title="Departments" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-building"></i></a></li>
+        <li><a href="/staff/position" class="nav-link py-3" title="Positions" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-person-workspace"></i></a></li>
+        <li><a href="/staff/message" class="nav-link py-3" title="Messages" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-chat-left-text"></i></a></li>
+        <li><a href="/staff/notification" class="nav-link py-3" title="Notifications" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-bell"></i></a></li>
       </ul>
       <ul>
         <li><a href="/logout" class="nav-link pt-4 mt-3" title="Logout" data-bs-toggle="tooltip" data-bs-placement="right"><i class="bi bi-box-arrow-left"></i></a></li>
