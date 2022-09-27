9d0e25aebe5cae3b80301e1cf89e3863785b632e
Daniel Salipot <danielsalipot@gmail.com>
2022-09-27 13:35:05 +0800

    db fix

--- database/migrations/2022_08_19_045400_create_employee_activities_table.php
+++ database/migrations/2022_08_19_045400_create_employee_activities_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('employee_activities', function (Blueprint $table) {
             $table->id();
-            $table->string('employee_id');
+            $table->bigInteger('employee_id');
             $table->longText('description');
             $table->dateTime('activity_date');
             $table->timestamps();
