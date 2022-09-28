ad411ea207b7739e1ba3307a34a289b18244e24d
Daniel Salipot <danielsalipot@gmail.com>
2022-09-29 00:57:13 +0800

    fix

--- routes/Admin/insert.php
+++ routes/Admin/insert.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\AdminInsertController;
 
 Route::prefix('')->group(function () {
-    Route::post('/insertLesson', [AdminInsertController::class, 'insertLesson']);
+    Route::get('/insertLesson', [AdminInsertController::class, 'insertLesson']);
     Route::post('/enrollEmployee', [AdminInsertController::class, 'enrollEmployee']);
     Route::post('/addAssessment', [AdminInsertController::class, 'addAssessment']);
     Route::post('/uploadLegalFormFiles', [AdminInsertController::class, 'uploadLegalFormFiles']);
